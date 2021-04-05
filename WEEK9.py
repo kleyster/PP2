784788bb7d0ab45972a1cf9b120fd51c58af6f5c
kleyster <aubakirof@gmail.com>
2021-04-06 00:05:02 +0600

    week 9

--- WEEK9.py
+++ WEEK9.py
@@ -4,14 +4,20 @@ pygame.init()
 screen=pygame.display.set_mode([640, 480])
 screen.fill([0, 0, 0])
 sinPoints = []
+sinClosePoints = []
 cosPoints = []
 for x in range(0,640):
-	y = int(math.cos(x/640*4*math.pi)*200+240)
-	cosPoints.append([x,y])
+    y = int(math.cos(x/640*4*math.pi)*200+240)
+    cosPoints.append([x,y])
 for x in range(0, 640):
     y = int(math.sin(x/640 * 4 * math.pi) * 200 + 240)
-    sinPoints.append([x, y])
-pygame.draw.lines(screen, [0, 255, 0], False, sinPoints, 2)
+    if (x%2==0):
+        sinPoints.append([x, y])
+    else:
+        sinClosePoints.append([x,y])
+for start,end in zip(sinPoints,sinClosePoints):
+    pygame.draw.lines(screen, [0, 255, 0], False, (start,end), 2)
+
 pygame.draw.lines(screen, [255, 0, 0], False, cosPoints, 2)
 pygame.display.flip()
 while True:
